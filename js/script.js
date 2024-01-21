d16f7e007831275ef1cdc43047c57799157b061a
sushmitavaidya <sushmitavaidya30@gmail.com>
2024-01-21 13:51:21 +0530

    Final

--- js/script.js
+++ js/script.js
@@ -130,12 +130,3 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
-
-//Location
-// function myMap() {
-//   var mapProp = {
-//     center: new google.maps.LatLng(15.835203646807964, 74.50147263394092),
-//     zoom: 5,
-//   };
-//   var map = new google.maps.Map(document.getElementById("Location"), mapProp);
-// }
